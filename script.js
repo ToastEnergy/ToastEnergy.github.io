0936f6faf7699a2e14b501bfe329697a43ab63e0
ssebastianoo <sebastianogirotto05@gmail.com>
2021-08-17 01:51:54 +0200

    mobile

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function sendTo(url) {
   location.href = url;
 }
+
+window.onload = function() {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    document.getElementById("css").href = "mobile.css";
+  };
+};
