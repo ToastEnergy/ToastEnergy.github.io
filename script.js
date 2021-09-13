05bb5ca237897f2ff826375ffb8a11d27cdab2c4
seb <48064277+ssebastianoo@users.noreply.github.com>
2021-09-13 17:16:06 +0200

    removed mobile.css redirect

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function sendTo(url) {
   location.href = url;
 }
-
-window.onload = function() {
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.getElementById("css").href = "mobile.css";
-  };
-};
